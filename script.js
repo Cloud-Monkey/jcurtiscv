f84535a8dae27cda2f94300d8ed3afbac64f5d78
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-07-25 10:00:46 +0000

    added console log to togglemenu function

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function togglemenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+    console.log(icon.classList)
+    console.log(menu.classList)
 }
